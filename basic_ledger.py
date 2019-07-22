2a5fecb67d5d0f2e4eaaaf05f8d7fa217dad228d
qizheng.zhang <qizheng.zhang@anl.gov>
2019-07-22 14:46:29 -0500

    version 0.3 updated

--- basic_ledger.py
+++ basic_ledger.py
@@ -1,6 +1,8 @@
 import numpy as np
+import datetime
 import math
 import csv
+import sys
 
 class Expense:
     def __init__(self, expense_date, expense_title, expense_place,
@@ -11,7 +13,7 @@ class Expense:
         self.amount = expense_amount
         self.approach = expense_approach
     def description(self):
-        print("----------------------------------------")
+        system.separation_line_2()
         print("Expense Description")
         print("Date:", self.date)
         print("Title:", self.title)
@@ -28,37 +30,106 @@ class List:
         self.list_expense.append(new_expense)
         self.num_expense += 1
     def description(self):
-        print("========================================")
+        system.separation_line_1()
         print("List Description")
         print("Title:", self.title)
         print("Num of expenses:", self.num_expense)
         for single_expense in self.list_expense:
             single_expense.description()
-        print("========================================")
+        system.separation_line_1()
+    def write_to_csv(self):
+        with open('exp_ledger.csv', mode='w') as ledger:
+            ledger_writer = csv.writer(ledger, delimiter=',', quotechar='"',
+                                       quoting=csv.QUOTE_MINIMAL)
+            ledger_writer.writerow(['expense_id','Date','Title','Place',
+                                    'Amount','Approach'])
+            for single_expense in self.list_expense:
+                ledger_writer.writerow([self.list_expense.index(single_expense)+1,
+                                        single_expense.date,
+                                        single_expense.title,
+                                        single_expense.place,
+                                        single_expense.amount,
+                                        single_expense.approach])
+    # def read_from_csv(list_obj):
 
-def write_to_csv(list_obj):
-    with open('exp_ledger.csv', mode='w') as ledger:
-        ledger_writer = csv.writer(ledger, delimiter=',', quotechar='"',
-                                   quoting=csv.QUOTE_MINIMAL)
-        ledger_writer.writerow(['expense_id','Date','Title','Place',
-                                'Amount','Approach'])
-        for single_expense in list_obj.list_expense:
-            ledger_writer.writerow([list_obj.list_expense.index(single_expense)+1,
-                                    single_expense.date,
-                                    single_expense.title,
-                                    single_expense.place,
-                                    single_expense.amount,
-                                    single_expense.approach])
+class User:
+    def __init__(self):
+        self.active_flag = 1
+    def add_expense(self):
+        '''
+        # default expense for testing
+        dinner = Expense("7/22","dinner","nella",20,"credit")
+        # dinner.description()
+        this_week = List("this week's expenses")
+        #this_week.description()
+        this_week.append_expense(dinner)
+        #this_week.description()
+        this_week.write_to_csv()
+        '''
+
+        system.separation_line_2()
+        print("Where would you like to add the expense:",
+              "1: to a new ledger 2: to an existing ledger",
+              "b: back to last branch q: quit whole program", sep = '\n')
+        op = input()
+        if op == '1':
+            print("new ledger initializing under progress ......")
+        elif op == '2':
+            system.under_construction()
+        elif op == 'q':
+            system.terminate()
+        else:
+            system.display_error(0)
+
+        print("add_expense executed")
+
+class System:
+    def __init__(self):
+        self.sys_active = 1
+        self.time = str(datetime.datetime.now())
+    def welcome(self):
+        print("Welcome to my_ledger", "Current Version: 0.3",
+              "Author: Qizheng Zhang", sep = '\n')
+    def display_error(self,error_code):
+        if error_code == 0: # invalid_input
+            print("Invalid input. Please try again.")
+    def under_construction(self):
+        print("this function is under construction")
+    def separation_line_1(self):
+        print("========================================")
+    def separation_line_2(self):
+        print("----------------------------------------")
+    def terminate(self):
+        system.separation_line_1()
+        print("my_ledger closed")
+        sys.exit(0)
+    def op_branch(self):
+        # this might be modified using recursion
+        loop_flag = 1
+        while loop_flag:
+            system.separation_line_1()
+            print("Please choose what you would like to do:",
+                  "1: add an expense 2: view a ledger q: exit", sep = '\n')
+            op = input()
+            if op == '1':
+                user.add_expense()
+            elif op == '2':
+                system.under_construction()
+                continue
+            elif op == 'q':
+                system.terminate()
+            else:
+                system.display_error(0)
 
 def main():
-    dinner = Expense("7/22","dinner","nella",20,"credit")
-    # dinner.description()
+    system.welcome()
 
-    this_week = List("this week's expenses")
-    #this_week.description()
-    this_week.append_expense(dinner)
-    #this_week.description()
+    system.op_branch()
 
-    write_to_csv(this_week)
+    system.terminate()
+    # end of main
 
+# Below for global contents
+system = System()
+user = User()
 main()
